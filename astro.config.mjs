279cf79137b50260b37da9db31200a1587fae372
Sajad <zareisajad@protonmail.com>
2025-08-09 12:41:28 +0330

    added dev source

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    vite: {plugins: [tailwindcss()]},
+    site: 'https://zareisajad.github.io',
+});
